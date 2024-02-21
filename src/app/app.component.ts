779996846d25b97f2e325dd37cc20964896e99aa
HugoClemt <hugoclemt@gmail.com>
2024-02-21 23:16:06 +0100

    Mise en place de tailwinds + header + debut page home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, RouterModule],
+  template:`
+    <div class="px-6 py-4 w-full bg-gray-900 flex gap-6 text-white">
+        <a class="" routerLink="/">Home</a>
+        <a class="" routerLink="/casier">Casiers</a>
+        <a class="" routerLink="/rapport">Rapports</a>
+        <a class="" routerLink="/plainte">Plaintes</a>
+        <a class="" routerLink="/amendes">Amendes</a>
+        <a class="" routerLink="/groupes">Groupes</a>
+        <a class="" routerLink="/enquête">Enquête</a>
+        <a class="" routerLink="/profil">Profil</a>
+    </div>
+      <router-outlet />
+    `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  enabled: process.env.WEBPACK_DEV_SERVER === "true" && process.argv.join(" ").indexOf("build") !== -1,
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
   },
